1baab1b4db80253791553502e9df2268b42d4847
Tedi Lowney <8723645+Tedicode@users.noreply.github.com>
2026-08-29 23:40:02 -0400

    feat: add date picker defaulted to today - Basic screen layout with title, subtitle and date text plus DateTimePicker component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
 
 export default function App() {
+  const[selectedDate, setSelectedDate] = useState<Date>(new Date());
+
+  const onDateChange = (event: DateTimePickerEvent, date?: Date) => {
+    if(date){
+      setSelectedDate(date)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Near Earth Object Monitor</Text>
+      <Text style={styles.subtitle}>Powered by NASA’s Open APIs</Text>
+      <Text style={styles.dateText}>View asteroids for: {selectedDate.toDateString()}</Text>
+      <DateTimePicker value={selectedDate} mode="date" onChange={onDateChange} />
+ 
     </View>
   );
 }
@@ -13,8 +24,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightpink',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  dateText: {
+    fontSize: 16,
+    color: 'black',
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
