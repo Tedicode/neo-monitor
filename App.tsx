f235cbc17ef3377c67dc4624bd19257d4f274996
Tedi Lowney <8723645+Tedicode@users.noreply.github.com>
2026-08-30 18:11:50 -0400

    feat: call NASA API successfully
    Call API using API key + formatted date string
    Fire the effect as selected date changes
    Placeholder effect to console.log response

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker';
 
@@ -10,6 +10,31 @@ export default function App() {
       setSelectedDate(date)
     }
   }
+  const apiKey = process.env.EXPO_PUBLIC_NASA_API_KEY;
+
+  const fetchAsteroids = async (date: Date) => {
+    
+    const dateString = date.toISOString().split('T')[0];
+
+    try {
+      const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateString}&end_date=${dateString}&api_key=${apiKey}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching asteroids:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAsteroids(selectedDate)
+    .then(data => {
+      console.log('Asteroids:', data);
+    })
+    .catch(error => {
+      console.error('Error fetching asteroids:', error);
+    });
+  }, [selectedDate]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Near Earth Object Monitor</Text>
