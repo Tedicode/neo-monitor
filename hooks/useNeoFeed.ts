5f819374f9e1388c4b27bc397dab268057f1b033
Tedi Lowney <8723645+Tedicode@users.noreply.github.com>
2026-08-31 15:48:08 -0400

    fix: guard against redundant calls - Update hook dependency from date object to dateString - Guards against extra API calls on same date selection - Add fallback empty array where response data is set on state

--- hooks/useNeoFeed.ts
+++ hooks/useNeoFeed.ts
@@ -11,13 +11,13 @@ function formatDateForApi(date: Date): string {
 }
 
 export const useNeoFeed = (date: Date) => {
+    const dateString = formatDateForApi(date)
     const [neos, setNeos] = useState<NearEarthObject[]>([]);
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<null | string>(null)
 
 
     useEffect(() => {
-        const dateString = formatDateForApi(date)
         const apiKey = process.env.EXPO_PUBLIC_NASA_API_KEY;
         // const apiKey = 'an invalid api key';
         const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateString}&end_date=${dateString}&api_key=${apiKey}`
@@ -30,13 +30,13 @@ export const useNeoFeed = (date: Date) => {
             if(!res?.ok) throw new Error(`Fetching asteroids: ${res?.status}`)
             return res.json()
           })   
-          .then(data => setNeos(data.near_earth_objects[dateString]))
+          .then(data => setNeos(data.near_earth_objects[dateString] ?? []))
           .catch(error => {
             setError(error.message);
           }).finally(() => setLoading(false));
 
           
-        }, [date])
+        }, [dateString])
         
         return { neos, loading, error }
     
